f1668c688d72ce851b8a330ea46ccc8e53eb7794
Ruhullah517 <111290404+Ruhullah517@users.noreply.github.com>
2024-11-05 12:37:25 +0500

    port updated in server.js

--- server.js
+++ server.js
@@ -142,4 +142,5 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
