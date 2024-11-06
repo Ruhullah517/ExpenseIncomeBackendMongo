86697fb01e5906802007d2474e6c12746dfe84b4
Ruhullah517 <111290404+Ruhullah517@users.noreply.github.com>
2024-11-06 11:23:57 +0500

    add * path in server.js

--- server.js
+++ server.js
@@ -140,6 +140,8 @@ app.get('/accounts/:accountId/expenses', async (req, res) => {
 app.get('/', (req, res) => {
     res.status(200).send("Backend is running");
 });
-
+app.get('*', (req, res) => {
+    res.status(404).send("Route not found");
+});
 // Start server
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
